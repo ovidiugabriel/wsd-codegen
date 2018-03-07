527f392113a61ce5253f431adbda903901e852cb
Ovidiu <ovidiugabriel@gmail.com>
2018-03-07 16:39:50 +0200

    Create Node.cr

---
+++ Node.cr
@@ -0,0 +1,25 @@
+
+class Node
+    TYPE_FINISH = "finish"
+    TYPE_RETURN = "return"
+    TYPE_START  = "start"
+    TYPE_CREATE = "create"
+    TYPE_CALL   = "call"
+
+    def initialize(type, from, to, message)
+        if type
+            type_names = {
+                "-->-"  =>  TYPE_FINISH,
+                "-->"   =>  TYPE_RETURN,
+                "->+"   =>  TYPE_START,
+                "->*"   =>  TYPE_CREATE,
+                "->"    =>  TYPE_CALL
+            }
+            @type = type_names.has_key?(type) ? type_names[type] : type
+        end
+
+        @from = from       if from
+        @to = to           if to
+        @message = message if message
+    end
+end
