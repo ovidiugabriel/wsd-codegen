e4b87b3fd887cfd0a0da7bec2d49f7cb7911aaf3
Ovidiu <ovidiugabriel@gmail.com>
2018-03-07 16:56:45 +0200

    Update AltNode.cr

--- node/AltNode.cr
+++ node/AltNode.cr
@@ -2,7 +2,7 @@
 require "./Node"
 
 class AltNode < Node
-    def initialize(type, message)
+    def initialize(type : String, message : String)
         super(type, nil, nil, message)
     end
 end
