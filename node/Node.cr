78b68050df7f6891587257ef86c32459c9a11fa9
Ovidiu <ovidiugabriel@gmail.com>
2018-09-24 14:15:48 +0300

    Update Node.cr

--- node/Node.cr
+++ node/Node.cr
@@ -6,8 +6,8 @@ class Node
     TYPE_CREATE = "create"
     TYPE_CALL   = "call"
 
-    def initialize(type : String, from : String = nil, to : String = nil, message : String = nil)
-        if type
+    def initialize(arrow_type : String, from : String = nil, to : String = nil, message : String = nil)
+        if arrow_type
             type_names = {
                 "-->-"  =>  TYPE_FINISH,
                 "-->"   =>  TYPE_RETURN,
@@ -15,11 +15,11 @@ class Node
                 "->*"   =>  TYPE_CREATE,
                 "->"    =>  TYPE_CALL
             }
-            @type = type_names.has_key?(type) ? type_names[type] : type
+            @type = type_names.has_key?(arrow_type) ? type_names[arrow_type] : arrow_type
         end
 
-        @from = from       if from
-        @to = to           if to
+        @from    = from    if from
+        @to      = to      if to
         @message = message if message
     end
 end
