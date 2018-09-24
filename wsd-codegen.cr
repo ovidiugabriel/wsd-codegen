7aff012d4e30fe3c5eef592fe903169c6e11de8f
Ovidiu <ovidiugabriel@gmail.com>
2018-09-24 14:27:05 +0300

    Update wsd-codegen.cr

--- wsd-codegen.cr
+++ wsd-codegen.cr
@@ -1,8 +1,24 @@
 
-REGEX_TYPE_FINISH = /^\s*(.*)\s*(-->-)\s*(.*)\s*:\s*(.*)\s*$/
-
 # Finish occurrence {finish}
-matches = REGEX_TYPE_FINISH.match(line)
-if matches
+
+if matches = /^\s*(.*)\s*(-->-)\s*(.*)\s*:\s*(.*)\s*$/.match(line)
     n = [ matches[2], matches[1], matches[3], matches[4] ]
+
+# Return message {return}
+
+elseif matches = /^\s*(.*)\s*(-->)\s*(.*)\s*:\s*(.*)\s*$/.match(line)
+    n = [matches[2], matches[1], matches[3], matches[4] ]
+    
+# Start occurrence {start}
+elseif matches = /^\s*(.*)\s*(->\+)\s*(.*)\s*:\s*(.*)\s*$/.match(line)
+    n = [matches[2], matches[1], matches[3], matches[4] ]
+
+# Object creation message {create}    
+elseif matches = /^\s*(.*)\s*(->\*)\s*(.*)\s*:\s*(.*)\s*$/.match(line)
+    n = [matches[2], matches[1], matches[3], matches[4] ]
+
+# (Synchronous) message {call}
+elseif matches = /^\s*(.*)\s*(->)\s*(.*)\s*:\s*(.*)\s*$/.match(line)
+    n = [matches[2], matches[1], matches[3], matches[4] ]
+
 end
