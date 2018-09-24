ad20b396b80a2335938653e938f4f2673b406784
Ovidiu <ovidiugabriel@gmail.com>
2018-09-24 14:34:45 +0300

    Update wsd-codegen.cr

--- wsd-codegen.cr
+++ wsd-codegen.cr
@@ -6,19 +6,37 @@ if matches = /^\s*(.*)\s*(-->-)\s*(.*)\s*:\s*(.*)\s*$/.match(line)
 
 # Return message {return}
 
-elseif matches = /^\s*(.*)\s*(-->)\s*(.*)\s*:\s*(.*)\s*$/.match(line)
-    n = [matches[2], matches[1], matches[3], matches[4] ]
+elsif matches = /^\s*(.*)\s*(-->)\s*(.*)\s*:\s*(.*)\s*$/.match(line)
+    n = [ matches[2], matches[1], matches[3], matches[4] ]
     
 # Start occurrence {start}
-elseif matches = /^\s*(.*)\s*(->\+)\s*(.*)\s*:\s*(.*)\s*$/.match(line)
-    n = [matches[2], matches[1], matches[3], matches[4] ]
+elsif matches = /^\s*(.*)\s*(->\+)\s*(.*)\s*:\s*(.*)\s*$/.match(line)
+    n = [ matches[2], matches[1], matches[3], matches[4] ]
 
 # Object creation message {create}    
-elseif matches = /^\s*(.*)\s*(->\*)\s*(.*)\s*:\s*(.*)\s*$/.match(line)
-    n = [matches[2], matches[1], matches[3], matches[4] ]
+elsif matches = /^\s*(.*)\s*(->\*)\s*(.*)\s*:\s*(.*)\s*$/.match(line)
+    n = [ matches[2], matches[1], matches[3], matches[4] ]
 
 # (Synchronous) message {call}
-elseif matches = /^\s*(.*)\s*(->)\s*(.*)\s*:\s*(.*)\s*$/.match(line)
-    n = [matches[2], matches[1], matches[3], matches[4] ]
+elsif matches = /^\s*(.*)\s*(->)\s*(.*)\s*:\s*(.*)\s*$/.match(line)
+    n = [ matches[2], matches[1], matches[3], matches[4] ]
 
+elsif matches = /^(destroy)\s+(.*)$/.match(line)
+    destroy_node = [ matches[2] ]
+    
+elsif matches = /^(alt)\s+(.*)$/.match(line)
+    alt_node = [ matches[1], matches[2] ]
+    
+elsif matches = /^(else)\s+(.*)$/.match(line)
+    alt_node = [ matches[1], matches[2] ]
+    
+elsif matches = /^(end)$/.match(line)
+    end_node = 1
+    
+elsif matches = /^(opt)\s+(.*)$/.match(line)
+    alt_node = [ matches[1], matches[2] ]
+
+elsif matches = /^(loop)\s+(.*)$/.match(line)
+    alt_node = [ matches[1], matches[2] ]
+    
 end
