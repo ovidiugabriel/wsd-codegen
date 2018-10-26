012fd63a5867bd9f2c84d79b1256db58fcaa7822
Ovidiu <ovidiugabriel@gmail.com>
2018-10-26 09:21:51 +0300

    Update CppBackend.cr

--- backend/CppBackend.cr
+++ backend/CppBackend.cr
@@ -2,6 +2,11 @@
 class CppBackend
     TAB = "    "
 
+    # singleton
+    def self.instance()
+        @@instance ||= new
+    end    
+    
     def generate_create_instance(class_name : String, object_name : String = nil) : String
         object_name = class_name.downcase() if !object_name
         return "#{class_name}* #{object_name} = new #{class_name}()"
